c60b070885a327bab12c23230a15a0c799dfdb35
woflsl11 <40293131+woflsl11@users.noreply.github.com>
2018-07-06 23:25:42 +0900

    Add files via upload

---
+++ webProject/Bjin_tip/팁.js
@@ -0,0 +1,11 @@
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        //x.previousElementSibling.className += " w3-red";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className = 
+        //x.previousElementSibling.className.replace(" w3-red", "");
+    }
+}
